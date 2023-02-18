9690c431740467f4af509a27823895f2b552b6b9
m3ro1986 <m3ro_1986@outlook.com>
2023-02-18 09:50:38 -0600

    pokemons

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -24,6 +24,9 @@ const Pokemons = () => {
                     ))
                 }
             </main>
+            <footer>
+                <p>Create by Mero</p>
+            </footer>
         </div>
     );
 };
